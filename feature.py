bde3cba9d8214f39d7eb044f809b89f0eba7c5b8
Sam Lea <sam.lea@aviatnet.com>
2019-04-30 21:52:38 +1200

    Update version 2 lib to get feature class instance

--- feature.py
+++ feature.py
@@ -1,3 +1,17 @@
 
-def get_something():
-    return "This is version 2 of the library"
+class Feature(object):
+    def __init__(self, version):
+        self.version = version
+        self.id = None
+
+    def set_id(self, id):
+        print("Setting ID v2 for version {}".format(self.version))
+        self.id = id
+
+    def get_id(self):
+        print("Getting ID v2 for version {}".format(self.version))
+        return self.id
+
+
+def get_feature(version):
+    return Feature(version)
